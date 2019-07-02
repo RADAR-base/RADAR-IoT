e9c2afa23967e4b7534b7453a30c1583657b831c
yatharthranjan <yatharthranjan89@gmail.com>
2019-07-02 15:26:21 +0100

    Add customisable logging support

---
+++ commons/log.py
@@ -0,0 +1,13 @@
+import logging
+
+
+def setup_custom_logger(name, level):
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+    return logger
