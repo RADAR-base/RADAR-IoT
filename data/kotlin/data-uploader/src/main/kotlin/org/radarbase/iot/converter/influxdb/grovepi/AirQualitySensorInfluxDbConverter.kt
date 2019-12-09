5dd5c7fe583f5d9625cf6bd22aee4409f02b9445
yatharthranjan <yatharthranjan89@gmail.com>
2019-12-09 11:48:37 +0000

    Adds grovepi air quality sensor converters

--- data/kotlin/data-uploader/src/main/kotlin/org/radarbase/iot/converter/influxdb/grovepi/AirQualitySensorInfluxDbConverter.kt
+++ data/kotlin/data-uploader/src/main/kotlin/org/radarbase/iot/converter/influxdb/grovepi/AirQualitySensorInfluxDbConverter.kt
@@ -1,27 +1,26 @@
-package org.radarbase.iot.converter.influxdb.coralenviro
+package org.radarbase.iot.converter.influxdb.grovepi
 
 import com.fasterxml.jackson.core.type.TypeReference
 import org.influxdb.dto.Point
 import org.radarbase.iot.commons.util.Parser
 import org.radarbase.iot.converter.influxdb.InfluxDbConverter
 import org.radarbase.iot.converter.messageparser.JsonMessageParser
-import org.radarbase.iot.sensor.CoralEnviroLight
+import org.radarbase.iot.sensor.GrovePiAirQualitySensor
 import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
 
-class CoralEnviroLightInfluxDbConverter(
-    private val measurementName: String = "coralEnviroLight",
+class AirQualitySensorInfluxDbConverter(
+    private val measurementName: String = "grovePiAirQuality",
     private val parser: Parser<String,
-            List<CoralEnviroLight>> = JsonMessageParser(typeReference)
-) :
-    InfluxDbConverter {
-
+            List<GrovePiAirQualitySensor>> = JsonMessageParser(typeReference)
+) : InfluxDbConverter {
     override fun convert(messages: List<String>): List<Point> {
         return messages.map { message ->
             parser.parse(message).map {
                 Point.measurement(measurementName)
                     .time(it.getTime().toLong(), TimeUnit.SECONDS)
                     .addField("value", it.getValue())
+                    .addField("airQuality", it.getAirQuality())
                     .tag(InfluxDbConverter.genericKeyMap)
                     .build()
             }
@@ -31,9 +30,8 @@ class CoralEnviroLightInfluxDbConverter(
     companion object {
 
         private val logger =
-            LoggerFactory.getLogger(CoralEnviroHumidityInfluxDbConverter::class.java)
+            LoggerFactory.getLogger(AirQualitySensorInfluxDbConverter::class.java)
 
-        private val typeReference = object : TypeReference<List<CoralEnviroLight>>() {}
+        private val typeReference = object : TypeReference<List<GrovePiAirQualitySensor>>() {}
     }
-
 }
