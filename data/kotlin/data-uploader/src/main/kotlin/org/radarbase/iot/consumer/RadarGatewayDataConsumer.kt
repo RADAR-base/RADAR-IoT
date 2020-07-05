ecbaa1abdfad5ea4bd6ced1564aa7cb86913a5c0
yatharthranjan <yatharthranjan89@gmail.com>
2020-07-05 19:45:19 +0100

    Fix RadarGatewayDataConsumer auth

--- data/kotlin/data-uploader/src/main/kotlin/org/radarbase/iot/consumer/RadarGatewayDataConsumer.kt
+++ data/kotlin/data-uploader/src/main/kotlin/org/radarbase/iot/consumer/RadarGatewayDataConsumer.kt
@@ -10,64 +10,62 @@ import org.radarbase.iot.sender.KafkaAvroDataSender
 import org.slf4j.LoggerFactory
 
 open class RadarGatewayDataConsumer : RestProxyDataConsumer {
-    private val kafkaDataSender: KafkaAvroDataSender
 
     constructor(
         uploadIntervalSeconds: Int,
         maxCacheSize: Int,
         kafkaDataSender: KafkaAvroDataSender
-    ) : super(uploadIntervalSeconds, maxCacheSize) {
-        this.kafkaDataSender = kafkaDataSender
-    }
+    ) : super(uploadIntervalSeconds, maxCacheSize, kafkaDataSender)
 
     constructor(
         uploadIntervalSeconds: Int,
         maxCacheSize: Int
-    ) : super(uploadIntervalSeconds, maxCacheSize) {
-        val mpUrl = "${CONFIGURATION.radarConfig
-            .baseUrl}/${CONFIGURATION.radarConfig.managementPortalPath}"
-
-        val managementPortalClient = ManagementPortalClient(
-            clientId = checkNotNull(CONFIGURATION.radarConfig.oAuthClientId),
-            clientSecret = checkNotNull(CONFIGURATION.radarConfig.oAuthClientSecret),
-            managementPortal = ServerConfig(mpUrl)
-        )
-
-        val nitriteProperties =
-            CONFIGURATION.persistenceStoreproperties
-                ?: PersistentOAuthStateStore.NitriteProperties(
-                    filePath = "/usr/local/radar/iot/oauthStore",
-                    username = null,
-                    password = null
-                )
-
-        val metaTokenUrl = "${mpUrl}/api/meta-token/${checkNotNull(
-            CONFIGURATION.radarConfig
-                .metaToken
-        )}"
-
-        logger.info("Meta token URL is: $metaTokenUrl")
-
-        this.kafkaDataSender = KafkaAvroDataSender(
-            authorizer = ManagementPortalAuthorizer(
-                userId = CONFIGURATION.radarConfig.userId,
-                managementPortalClient = managementPortalClient,
-                loginStrategy = MetaTokenLoginStrategy(
-                    metaTokenUrl,
-                    managementPortalClient
-                ),
-                oAuthStateStore = PersistentOAuthStateStore(nitriteProperties),
-                sourceId = CONFIGURATION.radarConfig.sourceId,
-                sourceTypeModel = CONFIGURATION.radarConfig.sourceTypeModel,
-                sourceTypeProducer = CONFIGURATION.radarConfig.sourceTypeProducer,
-                sourceTypeCatalogVersion = CONFIGURATION.radarConfig.sourceTypeCatalogVersion
-            ),
-            schemaUrl = CONFIGURATION.radarConfig.schemaRegistryUrl,
-            kafkaUrl = CONFIGURATION.radarConfig.kafkaUrl
-        )
-    }
+    ) : super(uploadIntervalSeconds, maxCacheSize, kafkaSender)
 
     companion object {
         private val logger = LoggerFactory.getLogger(this::class.java)
+        private val kafkaSender: KafkaAvroDataSender by lazy {
+            val mpUrl = "${CONFIGURATION.radarConfig
+                .baseUrl}/${CONFIGURATION.radarConfig.managementPortalPath}"
+
+            val managementPortalClient = ManagementPortalClient(
+                clientId = checkNotNull(CONFIGURATION.radarConfig.oAuthClientId),
+                clientSecret = checkNotNull(CONFIGURATION.radarConfig.oAuthClientSecret),
+                managementPortal = ServerConfig(mpUrl)
+            )
+
+            val nitriteProperties =
+                CONFIGURATION.persistenceStoreproperties
+                    ?: PersistentOAuthStateStore.NitriteProperties(
+                        filePath = "/usr/local/radar/iot/oauthStore",
+                        username = null,
+                        password = null
+                    )
+
+            val metaTokenUrl = "${mpUrl}/api/meta-token/${checkNotNull(
+                CONFIGURATION.radarConfig
+                    .metaToken
+            )}"
+
+            logger.info("Meta token URL is: $metaTokenUrl")
+
+            KafkaAvroDataSender(
+                authorizer = ManagementPortalAuthorizer(
+                    userId = CONFIGURATION.radarConfig.userId,
+                    managementPortalClient = managementPortalClient,
+                    loginStrategy = MetaTokenLoginStrategy(
+                        metaTokenUrl,
+                        managementPortalClient
+                    ),
+                    oAuthStateStore = PersistentOAuthStateStore(nitriteProperties),
+                    sourceId = CONFIGURATION.radarConfig.sourceId,
+                    sourceTypeModel = CONFIGURATION.radarConfig.sourceTypeModel,
+                    sourceTypeProducer = CONFIGURATION.radarConfig.sourceTypeProducer,
+                    sourceTypeCatalogVersion = CONFIGURATION.radarConfig.sourceTypeCatalogVersion
+                ),
+                schemaUrl = CONFIGURATION.radarConfig.schemaRegistryUrl,
+                kafkaUrl = CONFIGURATION.radarConfig.kafkaUrl
+            )
+        }
     }
 }
